7df9173b601f619a00c574023ad9a6a56771d002
Tap Payments <o.rabie@tap.company>
2020-03-08 08:13:25 +0200

    1- New Framework for Tap Theme Manager 2020

---
+++ TapThemeManager2020/TapThemeManager2020/TapThemeManager2020.h
@@ -0,0 +1,19 @@
+//
+//  TapThemeManager2020.h
+//  TapThemeManager2020
+//
+//  Created by Osama Rabie on 08/03/2020.
+//  Copyright © 2020 Osama Rabie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapThemeManager2020.
+FOUNDATION_EXPORT double TapThemeManager2020VersionNumber;
+
+//! Project version string for TapThemeManager2020.
+FOUNDATION_EXPORT const unsigned char TapThemeManager2020VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapThemeManager2020/PublicHeader.h>
+
+
