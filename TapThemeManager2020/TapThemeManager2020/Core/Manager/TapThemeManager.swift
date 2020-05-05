49329ecc88998c50b9a568825b7e567d5b0e06be
Tap Payments <o.rabie@tap.company>
2020-05-05 12:52:21 +0200

    1- Providing one interface to apply a theme throught the app. 2- Read json file from any given bundle

--- TapThemeManager2020/TapThemeManager2020/Core/Manager/TapThemeManager.swift
+++ TapThemeManager2020/TapThemeManager2020/Core/Manager/TapThemeManager.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 import class UIKit.UIView
+import enum UIKit.UIUserInterfaceStyle
+
 public let TapThemeUpdateNotification = "TapThemeUpdateNotification"
 
 /// The brain class for managing the Tap Theme manager itself
@@ -22,6 +24,8 @@ public let TapThemeUpdateNotification = "TapThemeUpdateNotification"
     /// Defines a global variable to state if the theme is already applied,hence the subviews doesn't need to reset the theme again
     @objc public var themeAlreadyApplied: Bool = false
     
+    fileprivate static var lightModeTheme:NSDictionary?
+    fileprivate static var darkModeTheme:NSDictionary?
     /*///// Defines the current selected theme location
      // public fileprivate(set) static var currentThemePath: TapThemePath?*/
     
@@ -69,9 +73,26 @@ public extension TapThemeManager {
      */
     @objc class func setTapTheme(jsonName: String) {
         // Check if the file exists
-        guard let jsonPath = TapThemePath.themeJsonPath(fileName: jsonName) else {
+        guard let jsonDict = loadThemeDict(from: jsonName) else {
             print("TapThemeManager WARNING: Can't find json '\(jsonName)'")
             return
+        }
+       
+        // All good, now change the theme :)
+        self.setTapTheme(themeDict: jsonDict)
+    }
+    
+    /**
+     Converts a JSON file into a Theme Dictionary
+     - Parameter jsonName: The name of the needed json file.
+     - Parameter bundle:   The bundle that we will search for the json file in. Default is the main bundle
+     - Returns: The theme dictionary converted from the given json file
+     */
+    internal class func loadThemeDict(from jsonName:String,in bundle:Bundle = Bundle.main) -> NSDictionary? {
+        // Check if the file exists
+        guard let jsonPath = TapThemePath.themeJsonPath(fileName: jsonName, in: bundle) else {
+            print("TapThemeManager WARNING: Can't find json '\(jsonName)'")
+            return nil
         }
         // Check if the file is correctly parsable
         guard
@@ -79,10 +100,9 @@ public extension TapThemeManager {
             let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed),
             let jsonDict = json as? NSDictionary else {
                 print("TapThemeManager WARNING: Can't read json '\(jsonName)' at: \(jsonPath)")
-                return
+                return nil
         }
-        // All good, now change the theme :)
-        self.setTapTheme(themeDict: jsonDict)
+        return jsonDict
     }
     
     /**
@@ -96,33 +116,70 @@ public extension TapThemeManager {
         NotificationCenter.default.post(name: Notification.Name(rawValue: TapThemeUpdateNotification), object: nil)
     }
     
+    /**
+    - The method is responsible for setting the global theme for the whole app.
+    - Parameter lightModeDictTheme: The dictionary of the the theme we need to apply in the Light display mode
+    - Parameter darkModeDictTheme: The dictionary of the the theme we need to apply in the dark display mode
+    */
+    @objc class func setDefaultTapTheme(lightModeDictTheme:NSDictionary,darkModeDictTheme:NSDictionary) {
+        self.lightModeTheme = lightModeDictTheme
+        self.darkModeTheme = darkModeDictTheme
+        applyThemeBasedOnDisplayMode()
+    }
     
+    /**
+    - The method is responsible for setting the global theme for the whole app.
+    - Parameter lightModeJSONTheme: The json file of the the theme we need to apply in the Light display mode
+    - Parameter darkModeJSONTheme: The json file of the the theme we need to apply in the dark display mode
+    - Parameter bundle:   The bundle that we will search for the json file in. Default is the main bundle
+    */
+    @objc class func setDefaultTapTheme(lightModeJSONTheme:String,darkModeJSONTheme:String,in bundle:Bundle = Bundle.main) {
+        // Check if the file exists
+        guard let lightJSONDict = loadThemeDict(from: lightModeJSONTheme, in: bundle),
+              let darkJSONDict = loadThemeDict(from: darkModeJSONTheme, in: bundle) else {
+                print("TapThemeManager WARNING: Can't find json files '\(lightModeJSONTheme)' and '\(darkModeJSONTheme)'")
+                // As a fallback we set the default theme
+                setDefaultTapTheme()
+                return
+        }
+        
+        self.lightModeTheme = lightJSONDict
+        self.darkModeTheme = darkJSONDict
+        applyThemeBasedOnDisplayMode()
+    }
+    
+    /// This method is responsible for choosing which theme to apply based on the current display mode light or dark
+    internal class func applyThemeBasedOnDisplayMode() {
+        // Make sure all theme objects are given and set
+        guard let lightJSONDict = self.lightModeTheme,
+        let darkJSONDict = self.darkModeTheme else {
+            // Fallback to use the default theme
+           setDefaultTapTheme()
+           return
+        }
+        
+        // We decide which theme object to user based on the current userInterfaceStyle
+        (UIView().traitCollection.userInterfaceStyle == .dark) ? self.setTapTheme(themeDict: darkJSONDict) : self.setTapTheme(themeDict: lightJSONDict)
+    }
     
     /// This is the method that is used to apply the default Tap provided theme
     @objc class func setDefaultTapTheme() {
-        // Check if the default theme is already applied from the super views
-        if shared.themeAlreadyApplied { return }
-         // Check if the caller passed a dark mode support then we check if need to apply it
-        let themeFile:String = (UIView().traitCollection.userInterfaceStyle == .dark) ? "DefaultDarkTheme" : "DefaultLightTheme"
-       
-        // Check if the file exists // Based on the current display mode, we decide which default theme file we will use
-        // Defensive code to make sure all is loaded correctly
         let bundle:Bundle = Bundle(for: TapThemeManager.self)
-        guard let jsonPath = bundle.path(forResource: themeFile, ofType: "json") else {
-            print("TapThemeManager WARNING: Can't find json 'DefaultTheme'")
-            return
-        }
-        // Check if the file is correctly parsable
-        guard
-            let data = try? Data(contentsOf: URL(fileURLWithPath: jsonPath)),
-            let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed),
-            let jsonDict = json as? NSDictionary else {
-                print("TapThemeManager WARNING: Can't read json 'DefaultTheme' at: \(jsonPath)")
-                return
-        }
-        // All good, now change the theme :)
-        shared.themeAlreadyApplied = true
-        self.setTapTheme(themeDict: jsonDict)
+        setDefaultTapTheme(lightModeJSONTheme:"DefaultLightTheme",darkModeJSONTheme:"DefaultDarkTheme",in: bundle)
+    }
+    
+    
+    /**
+     Call this method when a change in userInterfaceStyle has been detected to apply the correct theme object accordingly
+     - Parameter mode: The new value of the userInterfaceStyle
+     */
+    @objc class func changeThemeDisplay(for mode:UIUserInterfaceStyle) {
+        // Check if we are not already applying the correct theme for theh new display mode, this will prevent duplication setting the same theme muultiple times when different views are active and firing this method
+        if ( lightModeTheme == self.currentTheme && mode == .light ) ||
+            ( darkModeTheme == self.currentTheme && mode == .dark ) { return }
+        
+        // Here, we need to update the theme
+        applyThemeBasedOnDisplayMode()
     }
     
 }
